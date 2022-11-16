1dcde00d88844e33bb45fede020db59f93dacb9d
esignore <elena.signore@nttdata.com>
2022-11-16 11:08:25 +0100

    modified env variable name

--- src/services/dashboardRequestService.ts
+++ src/services/dashboardRequestService.ts
@@ -3,8 +3,8 @@ import { OnboardingRequestResource } from '../model/OnboardingRequestResource';
 import { mockedOnboardingRequests } from './__mocks__/dashboardRequestService';
 
 export const fetchOnboardingPspRequest = (tokenId: string): Promise<OnboardingRequestResource> => {
-  /* istanbul ignore if */
-  if (process.env.REACT_APP_API_MOCK_PARTY_USERS === 'true') {
+  /* istanbul ignore if */ 
+  if (process.env.REACT_APP_API_MOCK_PSP_REQUEST_DATA === 'true') {
     const selectedOnboardingRequest = mockedOnboardingRequests.find((r) => r.tokenId === tokenId);
     if (selectedOnboardingRequest) {
       return new Promise((resolve) => resolve(selectedOnboardingRequest));
