d40bbae76559db9af695791fa9b20081954cf0d9
Elena Signore <92723520+esignore@users.noreply.github.com>
2022-11-22 17:17:22 +0100

    SELC-1711 modified texts visualization

--- src/pages/confirmPage/ConfirmPage.tsx
+++ src/pages/confirmPage/ConfirmPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Stack, Typography } from '@mui/material';
 import { IllusCompleted } from '@pagopa/mui-italia';
 import { useTranslation, Trans } from 'react-i18next';
 import { OnboardingRequestResource } from '../../model/OnboardingRequestResource';
@@ -20,26 +20,28 @@ export default function ConfirmPage({ onboardingRequestData }: Props) {
       flexDirection="column"
       alignItems="center"
     >
-      <IllusCompleted size={60} />
-      <Grid item mt={3}>
-        <Typography variant="h4">{t('confirmPage.title')}</Typography>
-      </Grid>
-      <Grid item xs={4} display="flex" alignItems={'center'}>
-        <Typography variant="body1" textAlign="center">
-          <Trans i18nKey="confirmPage.description">
-            L’adesione di {{ ente: onboardingRequestData?.institutionInfo.name }} è stata approvata.
-            Invieremo all’indirizzo PEC indicato un’email con le istruzioni per completare
-            l’adesione.
-          </Trans>
-        </Typography>
-      </Grid>
-      <Button
-        variant="contained"
-        sx={{ alignSelf: 'center' }}
-        onClick={() => window.location.assign(ENV.URL_FE.LANDING)}
-      >
-        {t('confirmPage.backLabel')}
-      </Button>
+      <Stack mt={13} alignItems="center">
+        <IllusCompleted size={60} />
+        <Box mt={3}>
+          <Typography variant="h4">{t('confirmPage.title')}</Typography>
+        </Box>
+        <Grid item xs={5} display="flex" alignItems={'center'} mt={2}>
+          <Typography variant="body1" textAlign="center">
+            <Trans i18nKey="confirmPage.description">
+              L’adesione di {{ ente: onboardingRequestData?.institutionInfo.name }} è stata
+              approvata. Invieremo all’indirizzo PEC indicato un’email con le istruzioni per
+              completare l’adesione.
+            </Trans>
+          </Typography>
+        </Grid>
+        <Button
+          variant="contained"
+          sx={{ alignSelf: 'center', mt: 4 }}
+          onClick={() => window.location.assign(ENV.URL_FE.LANDING)}
+        >
+          {t('confirmPage.backLabel')}
+        </Button>
+      </Stack>
     </Grid>
   );
 }
