5d7d1e19f8f3b0d4981f7004f35007051b4cb3f5
esignore <elena.signore@nttdata.com>
2022-11-22 17:12:02 +0100

    SELC-1711 modified texts visualization

--- src/pages/rejectedPage/RejectPage.tsx
+++ src/pages/rejectedPage/RejectPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Stack, Typography } from '@mui/material';
 import { IllusCompleted } from '@pagopa/mui-italia';
 import { useTranslation, Trans } from 'react-i18next';
 import { OnboardingRequestResource } from '../../model/OnboardingRequestResource';
@@ -19,26 +19,28 @@ export default function RejectPage({ onboardingRequestData }: Props) {
       flexDirection="column"
       alignItems="center"
     >
-      <IllusCompleted size={60} />
-      <Grid item mt={3}>
-        <Typography variant="h4">{t('rejectPage.title')}</Typography>
-      </Grid>
-      <Grid item xs={4} display="flex" alignItems={'center'}>
-        <Typography variant="body1" textAlign="center">
-          <Trans i18nKey="rejectPage.description">
-            L’adesione di {{ ente: onboardingRequestData?.institutionInfo.name }} non è andata a
-            buon fine. Invieremo un’email all’indirizzo PEC indicato. Al suo interno, ci sono le
-            istruzioni per completare l’adesione.
-          </Trans>
-        </Typography>
-      </Grid>
-      <Button
-        variant="contained"
-        sx={{ alignSelf: 'center' }}
-        onClick={() => window.location.assign(ENV.URL_FE.LANDING)}
-      >
-        {t('rejectPage.backLabel')}
-      </Button>
+      <Stack mt={13} alignItems="center">
+        <IllusCompleted size={60} />
+        <Box mt={3}>
+          <Typography variant="h4">{t('rejectPage.title')}</Typography>
+        </Box>
+        <Grid item xs={5} display="flex" alignItems={'center'} mt={2}>
+          <Typography variant="body1" textAlign="center">
+            <Trans i18nKey="rejectPage.description">
+              L’adesione di {{ ente: onboardingRequestData?.institutionInfo.name }} non è andata a
+              buon fine. Invieremo un’email all’indirizzo PEC indicato. Al suo interno, ci sono le
+              istruzioni per completare l’adesione.
+            </Trans>
+          </Typography>
+        </Grid>
+        <Button
+          variant="contained"
+          sx={{ alignSelf: 'center', mt: 4 }}
+          onClick={() => window.location.assign(ENV.URL_FE.LANDING)}
+        >
+          {t('rejectPage.backLabel')}
+        </Button>
+      </Stack>
     </Grid>
   );
 }
