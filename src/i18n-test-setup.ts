710cbdc0470c0c0c04e3085b6909c30847e6d5f1
GabrieleMaiocchiFilippo <fmaiocch@emeal.nttdata.com>
2026-02-26 17:21:19 +0100

    update tranlsation setup test file

--- src/i18n-test-setup.ts
+++ src/i18n-test-setup.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import it from './locale/it';
 
-i18n.use(initReactI18next).init({
+void i18n.use(initReactI18next).init({
   lng: 'it',
   fallbackLng: 'it',
   ns: ['translation'],
