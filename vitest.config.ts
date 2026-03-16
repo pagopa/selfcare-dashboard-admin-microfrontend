55cc3a7e9460ca829f0e7b296c5800c0a49bf330
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2026-03-16 16:48:40 +0100

    fix unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    server: {
+      deps: {
+        inline: ['@pagopa/mui-italia'],
+      },
+    },
     setupFiles: ['./src/setupTests.ts'],
     exclude: ['**/node_modules/**'],
     coverage: {
