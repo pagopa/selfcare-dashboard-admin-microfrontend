36ac74c2935467cf44ea4e89de182b4443c3a729
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2026-05-13 16:49:15 +0200

    Create new routes for instituion detail page (#254)

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -11,8 +11,10 @@ export const ENV = {
   ROUTES: {
     ADMIN: `${PUBLIC_URL_INNER}/admin/onboarding`,
     ADMIN_SEARCH: `${PUBLIC_URL_INNER}/admin/search`,
+    ADMIN_SEARCH_DETAIL: `${PUBLIC_URL_INNER}/admin/search/:partyId`,
     ADMIN_PARTY_DETAIL: `${PUBLIC_URL_INNER}/admin/onboarding/:tokenId`,
     ADMIN_ONBOARDINGS: `${PUBLIC_URL_INNER}/admin/onboardings`,
+    ADMIN_ONBOARDINGS_DETAIL: `${PUBLIC_URL_INNER}/admin/onboardings/:partyId`,
   },
 
   DASHBOARD_ROUTES: {
