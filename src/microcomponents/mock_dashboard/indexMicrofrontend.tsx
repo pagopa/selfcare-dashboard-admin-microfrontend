575d36220042f901e5b4f75de2ba2b4652661794
OraldoDoci <oraldo.doci@nttdata.com>
2026-07-30 17:07:58 +0200

    update links for tos backstage

--- src/microcomponents/mock_dashboard/indexMicrofrontend.tsx
+++ src/microcomponents/mock_dashboard/indexMicrofrontend.tsx
@@ -29,6 +29,11 @@ CONFIG.URL_FE.LOGOUT = ENV.URL_FE.LOGOUT;
 // eslint-disable-next-line functional/immutable-data
 CONFIG.URL_FE.ASSISTANCE = ENV.URL_FE.ASSISTANCE;
 
+// eslint-disable-next-line functional/immutable-data
+CONFIG.FOOTER.LINK.PRIVACYPOLICY = `${CONFIG.FOOTER.LINK.PRIVACYPOLICY}?origin=backstage`;
+// eslint-disable-next-line functional/immutable-data
+CONFIG.FOOTER.LINK.TERMSANDCONDITIONS = `${CONFIG.FOOTER.LINK.TERMSANDCONDITIONS}?origin=backstage`;
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
