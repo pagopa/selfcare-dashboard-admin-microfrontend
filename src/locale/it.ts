10849adcf659e753d01a58190d007452db80ef3b
OraldoDoci <oraldo.doci@nttdata.com>
2026-09-22 16:12:15 +0200

    update Italian localization for adherence date

--- src/locale/it.ts
+++ src/locale/it.ts
@@ -63,7 +63,7 @@ export default {
       requestDateTooltip: 'Data in cui l’ente ha fatto la richiesta di adesione al prodotto',
       createdFromDate: 'Dal',
       createdToDate: 'Al',
-      updateDate: "DATA DI AGGIORNAMENTO DELL'ADESIONE",
+      updateDate: "DATA DI ADESIONE",
       updateDateTooltip: 'Data in cui il prodotto è stato attivato o disattivato',
       statusUpdatedFromDate: 'Dal',
       statusUpdatedToDate: 'Al',
